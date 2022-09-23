6ec624aae2fdaa9c9a07599d342117ed6fd7ccda
Anirudh Bhasin <67727931+Anirudh265@users.noreply.github.com>
2022-09-24 00:40:48 +0530

    Time: 141 ms (50.54%), Space: 6 MB (22.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -3,12 +3,18 @@ public:
     int concatenatedBinary(int n) {
         long long int mod=1e9+7;
         long long int prev=0;
+        //next sum becomes previous sum->leftshifted by the no of bits in the number + the number
+        //1->1
+        //2-> 1<<2+2=6
+        //3-> 6<<2+3=27
+        //basic logic is to just leftshift the previous sum by the number if bits in number and add the number to it
         for(int i=1;i<=n;++i)
         {
             int shift=int(log2(i));
             
             prev=(((prev<<shift+1)%mod)+i)%mod;
         }
+        
         return prev;
         
     }
