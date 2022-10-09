47497e5ebd2b06982dba4d389f7e4c761a591ed5
Anirudh Bhasin <67727931+Anirudh265@users.noreply.github.com>
2022-10-10 00:41:01 +0530

    Time: 18 ms (80.85%), Space: 17.9 MB (11.59%) - LeetHub

--- 735-asteroid-collision/735-asteroid-collision.cpp
+++ 735-asteroid-collision/735-asteroid-collision.cpp
@@ -5,24 +5,29 @@ public:
         
         for(int i=0;i<ast.size();++i)
         {
+            //if array is empty or the value or positive the there is no need for checking
             if(st.empty() or ast[i]>0)
             {
                 st.push(ast[i]);
             }
             else
             {
+                //if top is empty then add number regardless of sign because they would either be in opp direction or left
                 if(st.top()<0) {
                     st.push(ast[i]);
                     continue;
                 }
+                //remove postive values 
                 while(!st.empty() and abs(ast[i])>st.top() and st.top()>0)
                 {
                     st.pop();
                 }
+                //if it is empty or top is -ve so add the number
                  if(st.empty() or st.top()<0)
                  {
                      st.push(ast[i]);
                  }
+                //in case of equal values the stack will be popped once
                 else if(abs(ast[i])==st.top())st.pop();              
             }
             
